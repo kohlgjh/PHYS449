152baa1a7fefd4cb9380c7acaaf5e80987800b59
kohlgjh <kobro31@gmail.com>
2022-09-21 09:00:24 -0400

    Moved data and param extraction to own script

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,7 +1,6 @@
 import argparse
-import os
-import json
 import numpy as np
+from src.extractData import get_hyperparameters, get_input_data
 
 def parse_args():
     '''Parses command line args for input path and json path'''
@@ -11,32 +10,6 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def get_hyperparameters(args):
-    '''Extracts hyperparameters from json file passed in argparse.
-
-    Returns
-    ---
-    learning_rate: float
-        learning rate of the gradient descent
-    num_iter: int
-        number of iterations for gradient descent
-    '''
-    json_path = os.path.join(os.getcwd(), str(args.json_path))
-    with open(json_path) as json_file:
-        json_data = json.load(json_file)
-        return json_data['learning rate'], json_data['num iter']
-    
-def get_input_data(args):
-    '''Extracts input data from .in file passed in argparse.
-
-    Returns
-    ---
-    data_array: numpy array
-        array of the extracted data
-    '''
-    data_path = os.path.join(os.getcwd(), str(args.in_path))
-    data_array = np.loadtxt(data_path, delimiter=" ")
-    return data_array
 
 def main(args):
     '''Main entry point to assignment 1 code'''
---
+++ Assignment 1/src/extractData.py
@@ -0,0 +1,30 @@
+import os
+import json
+import numpy as np
+
+def get_hyperparameters(args):
+    '''Extracts hyperparameters from json file passed in argparse.
+
+    Returns
+    ---
+    learning_rate: float
+        learning rate of the gradient descent
+    num_iter: int
+        number of iterations for gradient descent
+    '''
+    json_path = os.path.join(os.getcwd(), str(args.json_path))
+    with open(json_path) as json_file:
+        json_data = json.load(json_file)
+        return json_data['learning rate'], json_data['num iter']
+    
+def get_input_data(args):
+    '''Extracts input data from .in file passed in argparse.
+
+    Returns
+    ---
+    data_array: numpy array
+        array of the extracted data
+    '''
+    data_path = os.path.join(os.getcwd(), str(args.in_path))
+    data_array = np.loadtxt(data_path, delimiter=" ")
+    return data_array
